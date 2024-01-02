ed504d962b0d3cfbb4653a31d84bf9590c7d9d17
ralphvirtucio <ralphvirtucio3@gmail.com>
2024-01-03 00:00:28 +0800

    Setup HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,84 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <header>
+        <h1>Job Listings</h1>
+      </header>
+
+      <main>
+        <section>
+          <div>
+            <ul>
+              <li>
+                <button>Frontend</button>
+              </li>
+              <li>
+                <button>CSS</button>
+              </li>
+              <li>
+                <button>JavaScript</button>
+              </li>
+            </ul>
+
+            <div>
+              <button>Clear</button>
+            </div>
+          </div>
+        </section>
+
+        <ul>
+          <li>
+            <div>
+              <div>
+                <img
+                  src=''
+                  alt=''
+                />
+              </div>
+
+              <div>
+                <div>
+                  <h2>Photosnap</h2>
+                  <span>New!</span>
+                  <span>Featured</span>
+                </div>
+
+                <div>
+                  <h3>Senior Frontend Developer</h3>
+                </div>
+
+                <div>
+                  <p>1d ago</p>
+                  <p>Full Time</p>
+                  <p>USA only</p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <ul>
+                <li>
+                  <button>Frontend</button>
+                </li>
+                <li>
+                  <button>Senior</button>
+                </li>
+                <li>
+                  <button>HTML</button>
+                </li>
+                <li>
+                  <button>CSS JavaScript</button>
+                </li>
+                <li>
+                  <button></button>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
 };
 
 export default App;
